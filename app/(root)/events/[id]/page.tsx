c63232d39f14610ece892cf97ca32f05097782d9
ZAID717 <bk17zaid@gmail.com>
2025-03-21 16:39:08 +0530

    route.ts updated

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -21,3 +21,16 @@
 // }
 
 // export default EventDetails
+
+
+import React from 'react'
+
+const EventDetails = () => {
+  return (
+    <div>
+      EventDetails
+    </div>
+  )
+}
+
+export default EventDetails
